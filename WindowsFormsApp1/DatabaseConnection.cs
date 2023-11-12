28f895362e475be2816d9f9545a3f1290cd37466
vunguyen140220@gmail.com <vunguyen140220@gmail.com>
2023-11-12 23:12:58 +0700

    feature-send-email-to-employees

---
+++ WindowsFormsApp1/DatabaseConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection connection;
+
+        public DatabaseConnection(string connectionString)
+        {
+            this.connection = new SqlConnection(connectionString);
+        }
+
+        public void Open()
+        {
+            connection.Open();
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+
+        public DataTable QueryData(string sql)
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+    }
+}
